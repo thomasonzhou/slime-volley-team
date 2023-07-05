78021555201f9e3bbfe62d53b1e85895d8ef1489
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2023-07-04 22:03:53 -0400

    Add stationary right agent

--- slimevolleygym/slimevolley.py
+++ slimevolleygym/slimevolley.py
@@ -541,6 +541,9 @@ class Game:
     self.fenceStub = None
     self.agent_left = None
     self.agent_right = None
+
+    self.agent_right_2 = None
+
     self.delayScreen = None
     self.np_random = np_random
     self.reset()
@@ -553,8 +556,14 @@ class Game:
     self.ball = Particle(0, REF_W/4, ball_vx, ball_vy, 0.5, c=BALL_COLOR);
     self.agent_left = Agent(-1, -REF_W/4, 1.5, c=AGENT_LEFT_COLOR)
     self.agent_right = Agent(1, REF_W/4, 1.5, c=AGENT_RIGHT_COLOR)
+
+    self.agent_right_2 = Agent(1, REF_W/5, 1.5, c=AGENT_RIGHT_COLOR)
+
     self.agent_left.updateState(self.ball, self.agent_right)
     self.agent_right.updateState(self.ball, self.agent_left)
+
+    self.agent_right_2.updateState(self.ball, self.agent_right_2)
+
     self.delayScreen = DelayScreen()
   def newMatch(self):
     ball_vx = self.np_random.uniform(low=-20, high=20)
@@ -568,6 +577,8 @@ class Game:
     self.agent_left.update()
     self.agent_right.update()
 
+    self.agent_right_2.update()
+
     if self.delayScreen.status():
       self.ball.applyAcceleration(0, GRAVITY)
       self.ball.limitSpeed(0, MAX_BALL_SPEED)
@@ -577,6 +588,10 @@ class Game:
       self.ball.bounce(self.agent_left)
     if (self.ball.isColliding(self.agent_right)):
       self.ball.bounce(self.agent_right)
+
+    if (self.ball.isColliding(self.agent_right_2)):
+      self.ball.bounce(self.agent_right_2)
+
     if (self.ball.isColliding(self.fenceStub)):
       self.ball.bounce(self.fenceStub)
 
@@ -609,6 +624,9 @@ class Game:
     canvas = self.fenceStub.display(canvas)
     canvas = self.agent_left.display(canvas, self.ball.x, self.ball.y)
     canvas = self.agent_right.display(canvas, self.ball.x, self.ball.y)
+
+    canvas = self.agent_right_2.display(canvas, self.ball.x, self.ball.y)
+
     canvas = self.ball.display(canvas)
     canvas = self.ground.display(canvas)
     return canvas
