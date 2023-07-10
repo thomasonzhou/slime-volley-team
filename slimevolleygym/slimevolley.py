63fd62c5d58249e9602f906e19359303876dd7de
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2023-07-10 17:13:17 -0400

    Restore libero legs

--- slimevolleygym/slimevolley.py
+++ slimevolleygym/slimevolley.py
@@ -577,8 +577,8 @@ class Game:
     self.agent_left_2.speedcap_y = self.agent_left_2.speedcap_y*1.25
 
     # libero
-    self.agent_right.speedcap_y = self.agent_right.speedcap_y*0.75
-    self.agent_left.speedcap_y = self.agent_left.speedcap_y*0.75
+    # self.agent_right.speedcap_y = self.agent_right.speedcap_y*0.75
+    # self.agent_left.speedcap_y = self.agent_left.speedcap_y*0.75
     
     self.delayScreen = DelayScreen()
   def newMatch(self):
